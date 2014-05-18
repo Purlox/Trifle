a98245a6207d4164b78f7adf8e923fc593b58121
Purlox <purlox@gmail.com>
2014-05-18 02:51:34 +0200

    Improved the way it works with other non-double types

--- trifle.h
+++ trifle.h
@@ -10,11 +10,40 @@
  * I decided to use percantages that make the float smaller to prevent
  *   potential overflows.
  */
+#  if defined __STDC_VERSION__ == 201112L
+
+#define COMPARE_FLOATS( float1, float2, percantage ) \
+  _Generic((float1), float: REAL_COMPARE_FLOATS, \
+                     double: REAL_COMPARE_FLOATS, \
+                     long double: REAL_COMPARE_FLOATS, \
+                     default: WRONG_TYPE)(float1, float2, percentage)
+#define REAL_COMPARE_FLOATS( float1, float2, percentage ) \
+  ( (float1) * percentage < (float2) && (float2) * percentage < (float1) )
+
+#define COMPARE_FLOATS_WITH_SET_PERCENTAGE( float1, float2 ) \
+  _Generic((float1), float: COMPARE_FLOATS_WITH_SET_PERCENTAGE_f, \
+                     double: COMPARE_FLOATS_WITH_SET_PERCENTAGE_d, \
+                     long double: COMPARE_FLOATS_WITH_SET_PERCENTAGE_ld, \
+                     default: WRONG_TYPE)(float1, float2)
+
+#define COMPARE_FLOATS_WITH_SET_PERCENTAGE_f( float1, float2 ) \
+  ( (float1) * 0.999f < (float2) && (float2) * 0.999f < (float1) )
+#define COMPARE_FLOATS_WITH_SET_PERCENTAGE_f( float1, float2 ) \
+  ( (float1) * 0.999 < (float2) && (float2) * 0.999 < (float1) )
+#define COMPARE_FLOATS_WITH_SET_PERCENTAGE_f( float1, float2 ) \
+  ( (float1) * 0.999L < (float2) && (float2) * 0.999L < (float1) )
+
+#define WRONG_TYPE( float1, float2 ) \
+  _Static_assert(1, "Wrong type(s) supplied to COMPARE_FLOATS");
+
+#  else
+
 #define COMPARE_FLOATS( float1, float2, percantage ) \
   ( (float1) * percentage < (float2) && (float2) * percentage < (float1) )
 
 #define COMPARE_FLOATS_WITH_SET_PERCENTAGE( float1, float2 ) \
   ( (float1) * 0.999 < (float2) && (float2) * 0.999 < (float1) )
 
+#  endif
 
 #endif // TRIFLE_H_INCLUDED
