c505e9a76bb743a693f5b848eb999088a0ba9649
Purlox <purlox@gmail.com>
2014-05-18 03:40:03 +0200

    Made them into functions to be safer.

--- trifle.h
+++ trifle.h
@@ -1,6 +1,8 @@
 #ifndef TRIFLE_H_INCLUDED
 #define TRIFLE_H_INCLUDED
 
+#include <stdbool.h>
+
 /* Compares two floats if they are /likely/ the same number.
  * Floats are approximations to real numbers anyway, so you should never
  *   compare them for exact equality with =, as they can be numbers that might
@@ -18,28 +20,44 @@
 #   if defined(SUPPORTS_GENERIC)
 
 #define COMPARE_FLOATS( float1, float2, percentage ) \
-  _Generic((float1), float: REAL_COMPARE_FLOATS, \
-                     double: REAL_COMPARE_FLOATS, \
-                     long double: REAL_COMPARE_FLOATS, \
+  _Generic((float1), float: compare_floats_f, \
+                     double: compare_floats_d, \
+                     long double: compare_floats_ld, \
                      default: WRONG_TYPE)(float1, float2, percentage)
-#define REAL_COMPARE_FLOATS( float1, float2, percentage ) \
-  ( (float1) * percentage < (float2) && (float2) * percentage < (float1) )
+
+bool compare_floats_f(float float1, float float2, float percentage) {
+  return ((float1 * percentage < float2) && (float2 * percentage < float1));
+}
+
+bool compare_floats_d(double float1, double float2, double percentage) {
+  return ((float1 * percentage < float2) && (float2 * percentage < float1));
+}
+
+bool compare_floats_ld(long double float1, long double float2, long double percentage) {
+  return ((float1 * percentage < float2) && (float2 * percentage < float1));
+}
 
 #define COMPARE_FLOATS_WITH_SET_PERCENTAGE( float1, float2 ) \
-  _Generic((float1), float: COMPARE_FLOATS_WITH_SET_PERCENTAGE_f, \
-                     double: COMPARE_FLOATS_WITH_SET_PERCENTAGE_d, \
-                     long double: COMPARE_FLOATS_WITH_SET_PERCENTAGE_ld, \
+  _Generic((float1), float: compare_floats_with_set_percentage_f, \
+                     double: compare_floats_with_set_percentage_d, \
+                     long double: compare_floats_with_set_percentage_ld, \
                      default: WRONG_TYPE)(float1, float2)
 
-#define COMPARE_FLOATS_WITH_SET_PERCENTAGE_f( float1, float2 ) \
-  ( (float1) * 0.999f < (float2) && (float2) * 0.999f < (float1) )
-#define COMPARE_FLOATS_WITH_SET_PERCENTAGE_f( float1, float2 ) \
-  ( (float1) * 0.999 < (float2) && (float2) * 0.999 < (float1) )
-#define COMPARE_FLOATS_WITH_SET_PERCENTAGE_f( float1, float2 ) \
-  ( (float1) * 0.999L < (float2) && (float2) * 0.999L < (float1) )
+bool compare_floats_with_set_percentage_f(float float1, float float2, float percentage) {
+  return ((float1 * 0.999f < float2) && (float2 * 0.999f < float1));
+}
+
+bool compare_floats_with_set_percentage_d(double float1, double float2, double percentage) {
+  return ((float1 * 0.999 < float2) && (float2 * 0.999 < float1));
+}
+
+bool compare_floats_with_set_percentage_ld(long double float1, long double float2,
+                                           long double percentage) {
+  return ((float1 * 0.999L < float2) && (float2 * 0.999L < float1));
+}
 
 #define WRONG_TYPE( float1, float2 ) \
-  _Static_assert(1, "Wrong type(s) supplied to COMPARE_FLOATS");
+  _Static_assert(true, "Wrong type(s) supplied to COMPARE_FLOATS");
 
 #   endif /* defined(SUPPORTS_GENERIC) */
 
